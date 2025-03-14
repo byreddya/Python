b6ff36b36cdf69220045c858582fc1b15eba8ad6
byreddya <byreddy.a@turing.com>
2025-03-14 23:16:47 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 print("Hello World!!")
-print("*" *10)
+print("*" * 10)
+num1 = float(input("Enter number 1:"))
+num2 = float(input("Enter number 2:"))
+sum_of_numbers = num1+num2
+product_of_sum = num1 * num2
+print("Sum of the number is:", sum_of_numbers)
+print("product of the number is:", product_of_sum)
+diff_nmu = num1-num2
+print("difference of numbers, num1-num2 is:", diff_nmu)
